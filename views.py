8d27f8f0fbd70280bcbeabf3ce18247e729bce39
Erick Ruh Cardozo <121986183+ErickRuhCardozo@users.noreply.github.com>
2025-11-17 17:36:20 -0300

    Refactor views.py and enhance functionality
    Refactor views.py to improve structure and add new features.

--- views.py
+++ views.py
@@ -1,11 +1,16 @@
+import asyncio
+from models import TrackedModel, BoxProduct
 from kivymd.app import MDApp
-from kivy.lang import Builder
 from kivymd.uix.screen import MDScreen
-from functools import partial
+from kivymd.uix.recycleview import MDRecycleView
+from kivymd.uix.list import OneLineAvatarIconListItem
 from kivy.clock import Clock
-from kivymd.uix.dialog import MDDialog
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.button import MDFlatButton
+from kivy.lang import Builder
+from kivymd.uix.list import (
+	MDList,
+	OneLineAvatarIconListItem,
+	IconLeftWidgetWithoutTouch,
+)
 from kivy.properties import (
 	ObjectProperty,
 	StringProperty,
@@ -13,131 +18,89 @@ from kivy.properties import (
 )
 from controllers import (
 	PalletController,
-	ControllerEvent,
 	BoxController,
+	ProductController,
 )
-from kivymd.uix.list import (
-	MDList,
-	OneLineAvatarIconListItem,
-	IconLeftWidgetWithoutTouch,
-)
+
+
+class ModelItem(OneLineAvatarIconListItem):
+	display_icon = StringProperty()
+	model = ObjectProperty()
+
+
+class ModelRecycleView(MDRecycleView):
+	controller = ObjectProperty()
+	item_icon = StringProperty()
+	when_item_pressed = ObjectProperty()
+	
+	def on_controller(self, _, controller):
+		controller.listen('added', self.when_added)
+		controller.listen('listed', self.when_listed)
+	
+	def get_dict(self, model):
+		d =  {
+			'model': model,
+			'display_icon': self.item_icon,
+			'when_item_pressed': self.when_item_pressed
+		}
+		
+		if isinstance(model, TrackedModel):
+			d['text'] = model.code
+		elif isinstance(model, BoxProduct):
+			d['text'] = model.product.name
+	
+		return d
+	
+	def when_added(self, model):
+		self.data.append(self.get_dict(model))
+	
+	def when_listed(self, models):
+		data = list(map(self.get_dict, models))
+		self.data = data
 
 
 class PalletsScreen(MDScreen):
-	pass
-
-
-class PalletDetailsScreen(MDScreen):
-	pallet_dto = DictProperty()
-	code = StringProperty()
-	created_at = StringProperty()
-	finished_at = StringProperty()
-
-	def on_enter(self, *args):
-		App.get_running_app().box_controller.list_boxes(self.pallet_dto['id'])
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		App.get_running_app().pallet_controller.listen('listed', self.when_pallets_listed)
 	
-	def on_leave(self, *args):
-		self.ids.boxes_list.clear_widgets()
-	
-	def on_pallet_dto(self, sender, pallet_dto):
-		self.code = pallet_dto['code']
-		controller = App.get_running_app().pallet_controller
-		controller.listen(ControllerEvent.DETAILS_LOADED, self.when_details_loaded)
-		controller.load_details(pallet_dto['id'])
-	
-	def when_details_loaded(self, details_dto):
-		self.created_at = details_dto['created_at']
-		self.finished_at = details_dto['finished_at']
-	
-	def hide_spinner(self):
-		self.ids.spinner.active = False
+	def when_pallets_listed(self, pallets):
+		self.ids.container.remove_widget(self.ids.pallets_spinner)
 
 
-class PalletsList(MDList):
+class DetailsScreen(MDScreen):
+	model = ObjectProperty()
+	title = StringProperty('Título')
+	inside_text = StringProperty()
+	code = StringProperty('Código')
+	created_at = StringProperty('Criação')
+	finished_at = StringProperty('Finalização')
+	go_back_to = StringProperty()
 	controller = ObjectProperty()
-	
-	def on_controller(self, _, controller):
-		controller.listen(ControllerEvent.ADDED, self.when_added)
-		controller.listen(ControllerEvent.LISTED, self.when_listed)
+	item_icon = StringProperty()
+	when_item_pressed = ObjectProperty()
 
-	def when_added(self, pallet_dto):
-		self.add_item(pallet_dto)
+	def on_model(self, caller, model):			
+		self.code = model.code
+		self.created_at = model.created_at.strftime('%x')
+		self.finished_at = model.finished_at.strftime('%x') if model.finished_at else 'Não Finalizado'
 	
-	def when_listed(self, pallet_dtos):
-		for dto in pallet_dtos:
-			Clock.schedule_once(
-				lambda _, d=dto: self.add_item(d)
-			)
-	
-	def add_item(self, pallet_dto):
-		item = OneLineAvatarIconListItem(text=pallet_dto['code'])
-		icon = IconLeftWidgetWithoutTouch(icon='shipping-pallet')
-		item.on_release = partial(self.when_item_clicked, pallet_dto)
-		item.add_widget(icon)
-		Clock.schedule_once(
-			lambda _, i=item: self.add_widget(i)
-		)
-	
-	def when_item_clicked(self, pallet_dto):
-		App.get_running_app().inspect_pallet(pallet_dto)
-
-
-class BoxDetailsScreen(MDScreen):
-	box_dto = DictProperty()
-	code = StringProperty()
-	created_at = StringProperty()
-	finished_at = StringProperty()
-
-	def on_box_dto(self, sender, box_dto):
-		self.code = box_dto['code']
-		controller = App.get_running_app().box_controller
-		controller.listen(ControllerEvent.DETAILS_LOADED, self.when_details_loaded)
-		controller.load_details(box_dto['id'])
-	
-	def when_details_loaded(self, details_dto):
-		self.created_at = details_dto['created_at']
-		self.finished_at = details_dto['finished_at']
-
-
-class BoxesList(MDList):
-	controller = ObjectProperty()
-	
-	def on_controller(self, sender, controller):
-		controller.listen(ControllerEvent.LISTED, self.when_listed)
-		controller.listen(ControllerEvent.ADDED, self.when_added)
-	
-	def when_added(self, box_dto):
-		self.add_item(box_dto)
-	
-	def when_listed(self, box_dtos):
-		for dto in box_dtos:
-			Clock.schedule_once(
-				lambda _, d=dto: self.add_item(d)
-			)
-	
-	def add_item(self, box_dto):
-		item = OneLineAvatarIconListItem(text=box_dto['code'])
-		icon = IconLeftWidgetWithoutTouch(icon='dropbox')
-		item.on_release = partial(self.when_item_clicked, box_dto)
-		item.add_widget(icon)
-		self.add_widget(item)
-	
-	def when_item_clicked(self, box_dto):
-		App.get_running_app().inspect_box(box_dto)
-
-
-class AddProductDialogContent(MDBoxLayout):
-	pass
+	def when_fab_press(self):
+		match self.name:
+			case 'pallet_details':
+				App.get_running_app().create_box(self.model)
+			case 'box_details':
+				print('ADD PRODUCT YO BOX')
 
 
 class App(MDApp):
-	def __init__(self):
+	def __init__(self, session_cls):
 		super().__init__()
-		self.add_product_dialog = None
-		self.pallet_controller = PalletController()
-		self.box_controller = BoxController()
-		self.pallet_controller.listen(ControllerEvent.ADDED, self.inspect_pallet)
-		self.box_controller.listen(ControllerEvent.ADDED, self.inspect_box)
+		self.pallet_controller = PalletController(session_cls)
+		self.box_controller = BoxController(session_cls)
+		self.product_controller = ProductController(session_cls)
+		self.pallet_controller.listen('added', self.inspect_pallet)
+		self.box_controller.listen('added', self.inspect_box)
 
 	def build(self):
 		self.theme_cls.theme_style = 'Dark'
@@ -145,40 +108,38 @@ class App(MDApp):
 		return Builder.load_file('ui.kv')
 	
 	def on_start(self):
-		self.pallet_controller.list_pallets()
+		Clock.schedule_once(
+			lambda dt:
+				asyncio.create_task(
+					self.pallet_controller.list()
+				),
+			2
+		)
 	
 	def create_pallet(self):
-		self.pallet_controller.create_pallet()
+		asyncio.create_task(
+			self.pallet_controller.add_new()
+		)
 	
-	def inspect_pallet(self, pallet_dto):
+	def create_box(self, pallet):
+		asyncio.create_task(
+			self.box_controller.add_new()
+		)
+	
+	def inspect_pallet(self, pallet):
+		self.box_controller.pallet = pallet
+		asyncio.create_task(self.box_controller.list())
 		screen = self.root.get_screen('pallet_details')
-		screen.pallet_dto = pallet_dto
-		self.go_to('pallet_details', 'left')
+		screen.model = pallet
+		self.go_to('pallet_details')
 	
-	def create_box(self, pallet_dto):
-		self.box_controller.create_box(pallet_dto)
-	
-	def inspect_box(self, box_dto):
+	def inspect_box(self, box):
+		self.product_controller.box = box
+		asyncio.create_task(self.product_controller.list())
 		screen = self.root.get_screen('box_details')
-		screen.box_dto = box_dto
-		self.go_to('box_details', 'left')
+		screen.model = box
+		self.go_to('box_details')
 	
-	def go_to(self, screen, direction):
+	def go_to(self, screen, direction='left'):
 		self.root.transition.direction = direction
 		self.root.current = screen
-	
-	def open_product_dialog(self):
-		if not self.add_product_dialog:
-			self.add_product_dialog = MDDialog(
-			title='Adicionar Produto',
-			type='custom',
-			content_cls=AddProductDialogContent(),
-			buttons=[
-				MDFlatButton(text='Adicionar')
-			]
-		)
-		self.add_product_dialog.open()
-
-
-if __name__ == '__main__':
-	App().run()
