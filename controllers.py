76dc366c1c0ce793fa6a28bec5b13e0a65e05e91
Erick Ruh Cardozo <121986183+ErickRuhCardozo@users.noreply.github.com>
2025-11-17 17:35:13 -0300

    Refactor controllers for async operations and Kivy integration
    Refactor controllers to use async/await pattern and Kivy for notifications.

--- controllers.py
+++ controllers.py
@@ -1,132 +1,97 @@
 from collections import defaultdict
-from enum import IntEnum, auto
-from sqlalchemy.orm import sessionmaker
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
 from models import (
-	Model,
 	Pallet,
-	Box
+	Box,
 )
 from sqlalchemy import (
 	select,
 	func,
-	create_engine,
 )
-
-
-ENGINE = create_engine(url='sqlite:///pallets.db', echo=False)
-Session = sessionmaker(bind=ENGINE, expire_on_commit=False)
-Model.metadata.create_all(ENGINE)
-
-
-class ControllerEvent(IntEnum):
-	ADDED = auto()
-	LISTED = auto()
-	DETAILS_LOADED = auto()
+from sqlalchemy.orm import selectinload
 
 
 class Controller:
-	def __init__(self):
+	def __init__(self, session_cls):
 		self.listeners = defaultdict(list)
+		self.session_cls = session_cls
 	
 	def listen(self, event, callback):
-		self.listeners[event].append(callback)
+		if callback not in self.listeners.get(event, []):
+			self.listeners[event].append(callback)
 	
 	def notify(self, event, *args, **kwargs):
 		for callback in self.listeners.get(event, []):
 			callback(*args, **kwargs)
+	
+	def connect(self):
+		return self.session_cls()
 
 
 class PalletController(Controller):
-	def create_pallet(self):
-		with Session() as session:
+	async def add_new(self):
+		async with self.connect() as session:
 			query = select(func.count()).select_from(Pallet)
-			count = session.scalar(query)
+			count = await session.scalar(query)
 			code = f'P{count + 1}'
 			pallet = Pallet(code=code)
 			session.add(pallet)
-			session.commit()
+			await session.commit()
 
-		dto = {'id': pallet.id, 'code': pallet.code}
-		self.notify(ControllerEvent.ADDED, dto)
+		Clock.schedule_once(lambda _, p=pallet: self.notify('added', p))
 	
-	def list_pallets(self):
-		with Session() as session:
-			query = select(Pallet.id, Pallet.code)
-			result = session.execute(query)
-			dtos = list(
-				map(
-					lambda x: {'id': x[0], 'code': x[1]},
-					result.all()
-				)
-			)
-
-		self.notify(ControllerEvent.LISTED, dtos)
-	
-	def load_details(self, pallet_id):
-		with Session() as session:
-			query = (
-				select(Pallet.created_at, Pallet.finished_at)
-				.where(Pallet.id == pallet_id)
-			)
-			result = session.execute(query)
-			created_at, finished_at = result.one()
+	async def list(self):
+		async with self.connect() as session:
+			query = select(Pallet)
+			result = await session.scalars(query)
+			pallets = result.all()
 		
-		dto = {
-			'created_at': created_at.strftime('%x'),
-			'finished_at': finished_at.strftime('%x') if finished_at else 'Não Finalizado'
-		}
-		self.notify(ControllerEvent.DETAILS_LOADED, dto)
+		Clock.schedule_once(lambda _, ps=pallets: self.notify('listed', ps))
 
 
 class BoxController(Controller):
-	def create_box(self, pallet_dto):
-		with Session() as session:
+	pallet = None
+	
+	async def add_new(self):
+		if self.pallet == None:
+			raise Exception('Pallet not set!')
+		
+		async with self.connect() as session:
 			query = (
 				select(func.count())
 				.select_from(Box)
-				.where(Box.pallet_id == pallet_dto['id'])
+				.where(Box.pallet_id == self.pallet.id)
 			)
-			count = session.scalar(query)
-			code = f'{pallet_dto["code"]}C{count + 1}'
-			box = Box(code=code, pallet_id=pallet_dto['id'])
+			count = await session.scalar(query)
+			code = f'{self.pallet.code}C{count + 1}'
+			box = Box(code=code, pallet_id=self.pallet.id)
 			session.add(box)
-			session.commit()
-			dto = {'id': box.id, 'code': code}
+			await session.commit()
 		
-		self.notify(ControllerEvent.ADDED, dto)
-
-	def list_boxes(self, pallet_id):
-		with Session() as session:
-			query = (
-				select(Box.id, Box.code)
-				.where(Box.pallet_id == pallet_id)
-			)
-			result = session.execute(query)
-			dtos = list(
-				map(
-					lambda x: {'id': x[0], 'code': x[1]},
-					result.all()
-				)
-			)
-		
-		self.notify(ControllerEvent.LISTED, dtos)
+		Clock.schedule_once(lambda _, b=box: self.notify('added', b))
 	
-	def load_details(self, box_id):
-		with Session() as session:
-			query = (
-				select(Box.created_at, Box.finished_at)
-				.where(Box.id == box_id)
-			)
-			result = session.execute(query)
-			created_at, finished_at = result.one()
-			
-		dto = {
-			'created_at': created_at.strftime('%x'),
-			'finished_at': finished_at.strftime('%x') if finished_at else 'Não Finalizada'
-		}
-		self.notify(ControllerEvent.DETAILS_LOADED, dto)
+	async def list(self):
+		if self.pallet == None:
+			raise Exception('Pallet not set')
+		
+		async with self.connect() as session:
+			pallet = await session.merge(self.pallet)
+			boxes = await pallet.awaitable_attrs.boxes
+		
+		Clock.schedule_once(lambda _, bs=boxes: self.notify('listed', bs))
 
 
-class BoxProductController(Controller):
-	def add_product(self):
-		pass
+class ProductController(Controller):
+	box = None
+	
+	async def list(self):
+		if self.box == None:
+			raise Exception('Box not set!')
+		
+		async with self.connect() as session:
+			box = await session.merge(self.box)
+			products = await box.awaitable_attrs.products
+			[await (await bp.awaitable_attrs.product).awaitable_attrs.name for bp in products] # Thafuck u were thinking?
+		
+		Clock.schedule_once(lambda _, ps=products: self.notify('listed', ps))
