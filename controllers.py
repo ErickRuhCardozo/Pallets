f1b6ce94c4ed2001d982f9a8c92476be039e511b
Erick Ruh Cardozo <121986183+ErickRuhCardozo@users.noreply.github.com>
2025-11-20 15:37:31 -0300

    Refactor controllers to use DTOs and add BoxProductController
    Refactor controllers to use DTOs for Pallet and Box, improving data handling and clarity. Added BoxProductController for managing BoxProduct entities.

--- controllers.py
+++ controllers.py
@@ -4,12 +4,14 @@ from kivy.properties import ObjectProperty
 from models import (
 	Pallet,
 	Box,
+	BoxProduct,
 )
 from sqlalchemy import (
 	select,
 	func,
 )
 from sqlalchemy.orm import selectinload
+from sqlalchemy.orm.exc import DetachedInstanceError
 
 
 class Controller:
@@ -39,7 +41,12 @@ class PalletController(Controller):
 			session.add(pallet)
 			await session.commit()
 
-		Clock.schedule_once(lambda _, p=pallet: self.notify('added', p))
+		dto = Pallet(
+			id=pallet.id,
+			code=code,
+			created_at=pallet.created_at
+		)
+		Clock.schedule_once(lambda _, p=dto: self.notify('added', p))
 	
 	async def list(self):
 		async with self.connect() as session:
@@ -47,11 +54,22 @@ class PalletController(Controller):
 			result = await session.scalars(query)
 			pallets = result.all()
 		
-		Clock.schedule_once(lambda _, ps=pallets: self.notify('listed', ps))
+		dtos = list(
+			map(
+				lambda p: Pallet(
+					id=p.id,
+					code=p.code,
+					created_at=p.created_at,
+					finished_at=p.finished_at
+				),
+				pallets
+			)
+		)
+		Clock.schedule_once(lambda _, ps=dtos: self.notify('listed', ps))
 
 
 class BoxController(Controller):
-	pallet = None
+	pallet: Pallet = None
 	
 	async def add_new(self):
 		if self.pallet == None:
@@ -68,30 +86,67 @@ class BoxController(Controller):
 			box = Box(code=code, pallet_id=self.pallet.id)
 			session.add(box)
 			await session.commit()
-		
-		Clock.schedule_once(lambda _, b=box: self.notify('added', b))
+
+		dto = Box(
+			id=box.id,
+			code=code,
+			created_at=box.created_at
+		)
+		Clock.schedule_once(lambda _, b=dto: self.notify('added', b))
 	
 	async def list(self):
 		if self.pallet == None:
 			raise Exception('Pallet not set')
 		
-		async with self.connect() as session:
-			pallet = await session.merge(self.pallet)
-			boxes = await pallet.awaitable_attrs.boxes
-		
-		Clock.schedule_once(lambda _, bs=boxes: self.notify('listed', bs))
+		if self.pallet.boxes != []:
+			dtos = self.pallet.boxes
+		else:
+			async with self.connect() as session:
+				pallet = await session.merge(self.pallet)
+				boxes = await pallet.awaitable_attrs.boxes
+				session.expunge_all()
+			
+			dtos = list(
+				map(
+					lambda b: Box(
+						id=b.id,
+						code=b.code,
+						created_at=b.created_at,
+						finished_at=b.finished_at
+					),
+					boxes
+				)
+			)
+			self.pallet.boxes = dtos
+
+		Clock.schedule_once(lambda _, bs=dtos: self.notify('listed', bs))
 
 
-class ProductController(Controller):
-	box = None
+class BoxProductController(Controller):
+	box: BoxProduct = None
 	
 	async def list(self):
 		if self.box == None:
 			raise Exception('Box not set!')
 		
-		async with self.connect() as session:
-			box = await session.merge(self.box)
-			products = await box.awaitable_attrs.products
-			[await (await bp.awaitable_attrs.product).awaitable_attrs.name for bp in products] # Thafuck u were thinking?
+		if self.box.products != []:
+			dtos = self.box.products
+		else:
+			async with self.connect() as session:
+				box = await session.merge(self.box)
+				products = await box.awaitable_attrs.products
+				dtos = []
+				
+				for bp in products:
+					product = await bp.awaitable_attrs.product
+					dtos.append(
+						BoxProduct(
+							id=bp.id,
+							quantity=bp.quantity,
+							product=product
+						)
+					)
+					
+			self.box.products = dtos
 		
-		Clock.schedule_once(lambda _, ps=products: self.notify('listed', ps))
+		Clock.schedule_once(lambda _, ps=dtos: self.notify('listed', ps))
