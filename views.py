36542dad71c0728433b8d6343275cf8a478d0a22
Erick Ruh Cardozo <121986183+ErickRuhCardozo@users.noreply.github.com>
2025-11-20 15:38:28 -0300

    Refactor views.py for BoxProduct and TrackedModel
    Refactor views.py to introduce new classes for BoxProduct and TrackedModel handling. Update methods for better data management and controller interactions.

--- views.py
+++ views.py
@@ -2,14 +2,15 @@ import asyncio
 from models import TrackedModel, BoxProduct
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.label import MDLabel
 from kivymd.uix.recycleview import MDRecycleView
-from kivymd.uix.list import OneLineAvatarIconListItem
+from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.clock import Clock
 from kivy.lang import Builder
 from kivymd.uix.list import (
-	MDList,
 	OneLineAvatarIconListItem,
-	IconLeftWidgetWithoutTouch,
+	TwoLineAvatarIconListItem,
+	IRightBody,
 )
 from kivy.properties import (
 	ObjectProperty,
@@ -19,16 +20,25 @@ from kivy.properties import (
 from controllers import (
 	PalletController,
 	BoxController,
-	ProductController,
+	BoxProductController,
 )
 
 
-class ModelItem(OneLineAvatarIconListItem):
-	display_icon = StringProperty()
+class TrackedModelItem(OneLineAvatarIconListItem):
+	icon = StringProperty()
 	model = ObjectProperty()
 
 
-class ModelRecycleView(MDRecycleView):
+class BoxProductItem(TwoLineAvatarIconListItem):
+	quantity = StringProperty()
+	model = ObjectProperty()
+
+
+class ProductQuantityLabel(IRightBody, MDLabel):
+	pass
+
+
+class ModelView(MDRecycleView):
 	controller = ObjectProperty()
 	item_icon = StringProperty()
 	when_item_pressed = ObjectProperty()
@@ -37,29 +47,39 @@ class ModelRecycleView(MDRecycleView):
 		controller.listen('added', self.when_added)
 		controller.listen('listed', self.when_listed)
 	
-	def get_dict(self, model):
-		d =  {
-			'model': model,
-			'display_icon': self.item_icon,
-			'when_item_pressed': self.when_item_pressed
-		}
-		
-		if isinstance(model, TrackedModel):
-			d['text'] = model.code
-		elif isinstance(model, BoxProduct):
-			d['text'] = model.product.name
-	
-		return d
-	
 	def when_added(self, model):
 		self.data.append(self.get_dict(model))
 	
 	def when_listed(self, models):
 		data = list(map(self.get_dict, models))
-		self.data = data
+		for dt in data:
+			Clock.schedule_once(
+				lambda _, d=dt: self.data.append(d)
+			)
 
 
-class PalletsScreen(MDScreen):
+class TrackedModelView(ModelView):	
+	def get_dict(self, model):
+		return  {
+			'text': model.code,
+			'model': model,
+			'icon': self.item_icon,
+			'when_item_pressed': self.when_item_pressed
+		}
+
+
+class BoxProductView(ModelView):
+	def get_dict(self, model):
+		return  {
+			'text': model.product.name,
+			'secondary_text': model.product.ean,
+			'quantity': str(model.quantity),
+			'model': model,
+			'when_item_pressed': self.when_item_pressed
+		}
+
+
+class PalletsScreen(MDScreen):	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		App.get_running_app().pallet_controller.listen('listed', self.when_pallets_listed)
@@ -70,27 +90,45 @@ class PalletsScreen(MDScreen):
 
 class DetailsScreen(MDScreen):
 	model = ObjectProperty()
-	title = StringProperty('Título')
-	inside_text = StringProperty()
 	code = StringProperty('Código')
 	created_at = StringProperty('Criação')
 	finished_at = StringProperty('Finalização')
-	go_back_to = StringProperty()
 	controller = ObjectProperty()
-	item_icon = StringProperty()
-	when_item_pressed = ObjectProperty()
-
+	
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.loading_spinner = None
+	
+	def on_kv_post(self, caller):
+		self.loading_spinner = self.ids.loading_spinner
+	
+	def on_leave(self, *args):
+		if App.get_running_app().can_hide_modelview(self.name):
+			self.ids.modelview.opacity = 0
+			self.ids.modelview.disabled = True
+			self.ids.modelview.data = []
+			self.ids.container.add_widget(self.loading_spinner, index=1)
+	
 	def on_model(self, caller, model):			
 		self.code = model.code
 		self.created_at = model.created_at.strftime('%x')
 		self.finished_at = model.finished_at.strftime('%x') if model.finished_at else 'Não Finalizado'
 	
-	def when_fab_press(self):
-		match self.name:
-			case 'pallet_details':
-				App.get_running_app().create_box(self.model)
-			case 'box_details':
-				print('ADD PRODUCT YO BOX')
+	def on_controller(self, caller, controller):
+		controller.listen('listed', self.when_controller_listed)
+	
+	def when_controller_listed(self, _):
+		self.ids.modelview.opacity = 1
+		self.ids.modelview.disabled = False
+		self.ids.container.remove_widget(self.loading_spinner)
+
+
+class PalletDetailsScreen(DetailsScreen):
+	pass
+
+
+class BoxDetailsScreen(DetailsScreen):
+	pass
 
 
 class App(MDApp):
@@ -98,7 +136,7 @@ class App(MDApp):
 		super().__init__()
 		self.pallet_controller = PalletController(session_cls)
 		self.box_controller = BoxController(session_cls)
-		self.product_controller = ProductController(session_cls)
+		self.boxproduct_controller = BoxProductController(session_cls)
 		self.pallet_controller.listen('added', self.inspect_pallet)
 		self.box_controller.listen('added', self.inspect_box)
 
@@ -112,34 +150,48 @@ class App(MDApp):
 			lambda dt:
 				asyncio.create_task(
 					self.pallet_controller.list()
-				),
-			2
+				)
 		)
 	
-	def create_pallet(self):
+	def add_pallet(self):
 		asyncio.create_task(
 			self.pallet_controller.add_new()
 		)
 	
-	def create_box(self, pallet):
+	def add_box(self, pallet):
 		asyncio.create_task(
 			self.box_controller.add_new()
 		)
 	
+	def show_product_dialog(self):
+		pass
+	
 	def inspect_pallet(self, pallet):
 		self.box_controller.pallet = pallet
-		asyncio.create_task(self.box_controller.list())
 		screen = self.root.get_screen('pallet_details')
 		screen.model = pallet
 		self.go_to('pallet_details')
+		asyncio.create_task(self.box_controller.list())
 	
 	def inspect_box(self, box):
-		self.product_controller.box = box
-		asyncio.create_task(self.product_controller.list())
+		print('inspecting box', box.id)
+		self.boxproduct_controller.box = box
 		screen = self.root.get_screen('box_details')
 		screen.model = box
 		self.go_to('box_details')
+		asyncio.create_task(self.boxproduct_controller.list())
 	
 	def go_to(self, screen, direction='left'):
 		self.root.transition.direction = direction
 		self.root.current = screen
+	
+	def can_hide_modelview(self, from_screen):
+		in_pallets = self.root.current == 'pallets'
+		in_pallet_details = self.root.current == 'pallet_details'
+		
+		if in_pallets and from_screen == 'pallet_details':
+			return True
+		elif in_pallet_details and from_screen == 'box_details':
+			return True
+		
+		return False
